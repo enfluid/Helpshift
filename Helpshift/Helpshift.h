7ec206b86d4a3e09645dda61c29f2ff0e8a74503
Maxim Nasakin <nasakinmaxim@gmail.com>
2017-10-07 12:53:02 +0300

    Initial commit

---
+++ Helpshift/Helpshift.h
@@ -0,0 +1,18 @@
+//
+//  Helpshift.h
+//  Helpshift
+//
+//  Created by Maxim Nasakin on 07/10/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Helpshift.
+FOUNDATION_EXPORT double HelpshiftVersionNumber;
+
+//! Project version string for Helpshift.
+FOUNDATION_EXPORT const unsigned char HelpshiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Helpshift/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Helpshift iOS SDK
+
+This repository contains [Helpshift iOS SDK](https://developers.helpshift.com) that can be used with [Carthage](https://github.com/Carthage/Carthage).
